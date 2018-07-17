ccc22d27ca0eda769809dbfe3407eff8d6bb2a2d
Evgeniy Khramkov <evkhramk@microsoft.com>
2018-07-17 21:58:50 +0300

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,7 +3,6 @@ which node
 node -v
 
 brew install node@8 -v
-echo 'export PATH="/usr/local/opt/node@8/bin:$PATH"' >> ~/.bash_profile
-exec bash
+export PATH="/usr/local/opt/node@8/bin:$PATH"
 
 node -v
