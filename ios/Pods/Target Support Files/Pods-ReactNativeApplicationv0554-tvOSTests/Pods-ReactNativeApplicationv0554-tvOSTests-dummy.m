5ddd94982b1b8e53680e64e0f827dc80e0f82872
Evgenii Khramkov <evkhramk@microsoft.com>
2018-09-14 17:10:11 +0300

    uncheck pods

--- ios/Pods/Target Support Files/Pods-ReactNativeApplicationv0554-tvOSTests/Pods-ReactNativeApplicationv0554-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeApplicationv0554_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeApplicationv0554_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-ReactNativeApplicationv0554Tests/Pods-ReactNativeApplicationv0554Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeApplicationv0554Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeApplicationv0554Tests
-@end
